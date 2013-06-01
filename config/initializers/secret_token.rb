18e1967318ed82891d60bdd652e175e0e5f1b319
pedrogrande <peter.argent@gmail.com>
2013-06-01 15:21:06 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Justin::Application.config.secret_token = '9fdd283a10543af6e9c80b426d9a032d66d170d3fc1fc98eeb9e4888212dde8f814fe3bf3edaabf8d8b0fd5869a8d061a512c1334d32c24c620f3832f1d36fbe'
